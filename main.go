465fcf95dae5188b3608aa803a247b51a3fdc967
Geoff 👌 Cowan <geoffroy.cowan@gmail.com>
2023-09-09 23:56:41 +1000

    tracer back

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"os"
+	"runtime/trace"
+)
+
 func main() {
+	traceFile, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer traceFile.Close()
+	trace.Start(traceFile)
+	defer trace.Stop()
+
 	r := &runner{}
 	if err := r.init(); err != nil {
 		panic(err)
