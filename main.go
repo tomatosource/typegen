b281cfc64ee126200a4a98ab6c9ef84c98eb2f1e
Jesse Colville <jesse.colville@eucalyptus.vc>
2023-09-12 11:46:30 +1000

    ,

--- main.go
+++ main.go
@@ -400,7 +400,7 @@ func (r *runner) genQueryType(query string) (string, string, error) {
 		"--go-uuid=github.com/google/uuid",
 		"--schema=public",
 		"--go-field-tag=db:\"{{ .SQLName }}\"",
-		"--allow-nulls"
+		"--allow-nulls",
 		fmt.Sprintf("--type=%s", queryName),
 		fmt.Sprintf("--query=%s", q),
 	)
